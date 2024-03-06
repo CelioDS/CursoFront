b2a75c8d58ad79eabae32ca02217235111ebce8e
celio <celio01t@gmail.com>
2024-03-06 19:48:01 -0300

    FEAT : mudança de layout, estilos e novas funcionalidades no localstorage

--- js/script.js
+++ js/script.js
@@ -2,29 +2,38 @@ const FormTarefa = document.getElementById("formulario");
 const listTarefa = document.getElementById("list-tarefa");
 const tarefa = document.getElementById("tarefa");
 
+const arrayTarefas = [];
+
+function GetTarefas() {
+  for (var i = 0; i < localStorage.length; i++) {
+    var key = localStorage.key(i);
+    var value = localStorage.getItem(key);
+
+    arrayTarefas.push(value);
+  }
+}
+console.log(arrayTarefas);
+
 FormTarefa.addEventListener("submit", (e) => {
   localStorage.setItem(tarefa.value, tarefa.value);
-  console.log("Enviado");
-  listarTarefas();
 });
 
 function listarTarefas() {
   for (var i = 0; i < localStorage.length; i++) {
     const div = document.createElement("div");
     const button = document.createElement("button");
-    const h3 = document.createElement("h3");
+    const h3 = document.createElement("h5");
 
     var chave = localStorage.key(i); // Obtém a chave atual
     var valor = localStorage.getItem(chave); // Obtém o valor associado à chave
-    console.log(localStorage.key(i));
 
     h3.innerText = valor;
     button.innerHTML = "&#120;";
 
     div.appendChild(h3);
     div.appendChild(button);
+    div.className = "divList"; /* excluir se não usar */
     listTarefa.appendChild(div);
   }
 }
-
 listarTarefas();
