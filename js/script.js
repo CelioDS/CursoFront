d7722ff657a5cffefdb5a56ffce73a67ca302b79
celio <celio01t@gmail.com>
2024-03-11 08:15:35 -0300

    FEAT : adiconado loading e funções curd

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
-const FormTarefa = document.getElementById("formulario");
+const AddTarefa = document.getElementById("formulario");
 const listTarefa = document.getElementById("list-tarefa");
 const tarefa = document.getElementById("tarefa");
+const Loading = document.getElementById("loading");
 
-FormTarefa.addEventListener("submit", (e) => {
+AddTarefa.addEventListener("submit", (e) => {
   const obj = {
     tarefa: tarefa.value,
     concluido: false,
@@ -14,17 +15,14 @@ FormTarefa.addEventListener("submit", (e) => {
 //localStorage.clear()
 
 function listarTarefas() {
-  for (var i = 0; i < localStorage.length; i++) {
+  for (let i = 0; i < localStorage.length; i++) {
     const h3 = document.createElement("h5");
     const div = document.createElement("div");
     const divButton = document.createElement("div");
     const buttonExcluir = document.createElement("button");
     const buttonConcluido = document.createElement("button");
 
-    div.className = "divList"; /* excluir se não usar */
     divButton.classList.add("divButton");
-
-    /** adcionar um class para o item quando concluido e deixa ele apgado */
 
     let chave = localStorage.key(i); // Obtém a chave atual
     let valor = JSON.parse(localStorage.getItem(chave)); // Obtém o valor associado à chave
@@ -33,32 +31,33 @@ function listarTarefas() {
     buttonExcluir.innerHTML = "&#120;";
     buttonConcluido.innerHTML = "&#10003;";
 
-    buttonConcluido.onclick = () => {
-      const obj = JSON.parse(localStorage.getItem(chave));
-      obj.concluido = !obj.concluido;
-      localStorage.setItem(obj.tarefa, JSON.stringify(obj));
-    };
-    buttonConcluido.onclick = () => {
-      const obj = JSON.parse(localStorage.getItem(chave));
-      obj.concluido = !obj.concluido;
-      localStorage.setItem(obj.tarefa, JSON.stringify(obj));
-    };
-
-    buttonExcluir.onclick = () => {
-      localStorage.removeItem(chave);
-    };
-
     if (valor.concluido) {
       div.classList.add("concluido");
     }
 
+    CRUD(buttonConcluido, buttonExcluir, chave);
+
     div.appendChild(h3);
+    div.appendChild(divButton);
+    listTarefa.appendChild(div);
     divButton.appendChild(buttonExcluir);
     divButton.appendChild(buttonConcluido);
-
-    div.appendChild(divButton);
-
-    listTarefa.appendChild(div);
+    
+    Loading.style.display = "none";
   }
 }
 listarTarefas();
+
+function CRUD(buttonConcluido, buttonExcluir, chave) {
+  buttonConcluido.onclick = () => {
+    const obj = JSON.parse(localStorage.getItem(chave));
+    obj.concluido = !obj.concluido;
+    localStorage.setItem(obj.tarefa, JSON.stringify(obj));
+    window.location.reload();
+  };
+
+  buttonExcluir.onclick = () => {
+    localStorage.removeItem(chave);
+    window.location.reload();
+  };
+}
