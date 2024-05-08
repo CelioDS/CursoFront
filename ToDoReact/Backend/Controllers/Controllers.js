997e22e545f308f2372c3f4963d35bf63a619f68
celio <celio01t@gmail.com>
2024-05-08 10:49:33 -0300

    FEAT : Backend finalizado falta fazer teste de requisições

--- ToDoReact/Backend/Controllers/Controllers.js
+++ ToDoReact/Backend/Controllers/Controllers.js
@@ -10,7 +10,43 @@ export const getDB = (_, res) => {
   });
 };
 
+export const setDB = (req, res) => {
+  const query = "INSERT INTO todoReact";
 
-export const setDB = (req, res) =>{
-  const query = "INSERT INTO todo"
-}
+  const values = [
+    req.body.tarefa,
+    req.body.concluido,
+    req.body.data,
+    req.body.quantidade,
+  ];
+  db.query(query, [values], (err) => {
+    if (err) return res.json(err);
+    return res.status(200).json("Cadastrado a tarefa");
+  });
+};
+
+export const updateDB = (req, res) => {
+  const query =
+    "UPDATE todoReact SET `tarefa` = ?, `concluido` = ? , `data` = ? , quantidade = ? WHERE `id` = ? ";
+
+  const values = [
+    req.body.tarefa,
+    req.body.concluido,
+    req.body.data,
+    req.body.quantidade,
+  ];
+  db.query(query, [...values, req.params.id], (err) => {
+    if (err) return res.json(err);
+
+    return res.status(200).json("Tarefa atualizada....");
+  });
+};
+
+export const deleteDB = (req, res) => {
+  const query = "DELETE FROM dados WHERE `id` = ?";
+
+  db.query(query, [req.params.id], (err) => {
+    if (err) return res.json(err);
+    return res.status(200).json("Tarefa deletada");
+  });
+};
