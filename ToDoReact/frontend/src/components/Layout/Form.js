b887be4fc3973c36b49b6b5d14ed9adc11c98f73
celio <celio01t@gmail.com>
2024-05-12 15:45:44 -0300

    FEAT : Correção no delete,post e get

--- ToDoReact/frontend/src/components/Layout/Form.js
+++ ToDoReact/frontend/src/components/Layout/Form.js
@@ -8,10 +8,10 @@ import "./Form.module.css";
 import Button from "../Item-Layout/Button";
 import Input from "../Item-Layout/Input";
 
-export default function Form() {
+export default function Form({ GetDB }) {
   const ref = useRef();
   const [currentDate, setCurrentDate] = useState("yyyy-MM-dd"); // Estado para controlar o valor do campo de data
-  const [submit, setSubmit] = useState(false);
+  const [IsSubmit, setIsSubmit] = useState(false);
 
   useEffect(() => {
     const today = new Date();
@@ -21,10 +21,13 @@ export default function Form() {
 
   async function handleSubmit(e) {
     e.preventDefault();
+    if (IsSubmit) return; // Impede o envio duplicado enquanto a requisição anterior ainda não foi concluída
+    setIsSubmit(true);
     const DadosForm = ref.current;
     console.log("clicou" + DadosForm.tarefa.value);
 
     if (!DadosForm.tarefa.value) {
+      setIsSubmit(false); // Reabilita o botão após o envio do formulário
       return toast.warn("Tarefa não foi informada...");
     } else {
       await axios
@@ -36,7 +39,9 @@ export default function Form() {
         .then(({ data }) => toast.success(data))
         .catch(({ data }) => toast.error(data));
     }
+    GetDB();
     DadosForm.tarefa.value = "";
+    setIsSubmit(false);
   }
   return (
     <form ref={ref} onSubmit={handleSubmit}>
@@ -46,7 +51,11 @@ export default function Form() {
         name="tarefa"
         placeholder="Digite a tarefa aqui..."
       />
-      <Button text={"Adicionar"} type={"submit"} />
+      <Button
+        text={IsSubmit ? "Adcionando..." : "Adicionar"}
+        type={"submit"}
+        disabled={IsSubmit}
+      />
     </form>
   );
 }
--- ToDoReact/frontend/src/components/Router/Home.js
+++ ToDoReact/frontend/src/components/Router/Home.js
@@ -27,7 +27,7 @@ export default function Home() {
   return (
     <Container>
       <h1>To Do List React</h1>
-      <Form />
+      <Form GetDB={GetDB} />
       <Table arrayDB={arrayDB} setArrayDB={setArrayDB} />
     </Container>
   );
