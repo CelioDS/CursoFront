63ae77a8c629f7c39475226806768f9208cea447
celio <celio01t@gmail.com>
2024-05-15 19:29:06 -0300

    STYLE : criando footer

--- ToDoReact/frontend/src/App.js
+++ ToDoReact/frontend/src/App.js
@@ -3,6 +3,7 @@ import { Outlet } from "react-router-dom";
 import Header from "./components/Layout/Header";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Footer from "./components/Layout/Footer";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         autoClose={3000}
         position="bottom-right"
       />
+      <Footer />
     </div>
   );
 }
