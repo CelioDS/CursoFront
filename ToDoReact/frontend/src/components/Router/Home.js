de9309a2773a7edb78ed1536cebcf7d0082fc278
Célio Da Silva <106108913+CelioDS@users.noreply.github.com>
2024-05-27 17:55:54 -0300

    Update Home.js

--- ToDoReact/frontend/src/components/Router/Home.js
+++ ToDoReact/frontend/src/components/Router/Home.js
@@ -5,6 +5,23 @@ import Header from "../Layout/Header";
 import "./Home.module.css";
 
 export default function Home() {
+
+
+useEffect(() => {
+    // Define o título da página
+    document.title = "Configuracao - fluxo de caixa";
+ 
+    // Função de limpeza para desconectar o efeito ao desmontar o componente
+    return () => {
+      document.title = "Página anterior"; // Restaura o título original (opcional)
+    };
+  }, []); // Array vazio indica que o efeito será executado apenas uma vez
+ 
+  return (
+    // Seu componente renderizado aqui
+  );
+}
+  
   return (
     <Container>
       <Header />
