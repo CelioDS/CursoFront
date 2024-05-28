1b68068d2e695d08e81d4a1df97305299b2ae25b
Célio Da Silva <106108913+CelioDS@users.noreply.github.com>
2024-05-28 10:28:14 -0300

    Create Title.js

---
+++ ToDoReact/frontend/src/components/Tools/Title.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react';
+ 
+export default function Title({nome}) {
+  useEffect(() => {
+    // Define o título da página
+    document.title = {nome};
+ 
+    // Função de limpeza para desconectar o efeito ao desmontar o componente
+    
+  }, []); // Array vazio indica que o efeito será executado apenas uma vez
+ 
+  
+}
+ 
